2fbe945748a14546d218c1d85c150e29baf17474
Brian R. Jackson <brian@jaxzin.com>
2019-02-18 20:29:52 -0500

    Bring in holoplay-gamepad via yarn.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,15 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         curl: {
-            'tmp/holoplay.zip': 'https://s3.amazonaws.com/static-files.lookingglassfactory.com/ThreeJSLibrary/HoloPlay.zip',
-            'tmp/GamePad.js.zip': 'https://github.com/jaxzin/GamePad.js/archive/add-holoplay.zip',
-            'lkg-viewer/js/deps/events/eventemitter.js': 'https://gist.githubusercontent.com/liamcurry/5818048/raw/b453a6967a0ce386d7b0b57e71405086686b00dd/eventemitter.js'
+            'tmp/holoplay.zip': 'https://s3.amazonaws.com/static-files.lookingglassfactory.com/ThreeJSLibrary/HoloPlay.zip'
         },
         unzip: {
-            'tmp/holoplay': 'tmp/holoplay.zip',
-            'tmp/GamePad': 'tmp/GamePad.js.zip'
+            'tmp/holoplay': 'tmp/holoplay.zip'
         },
         copy: {
             holoplay: {
                 files: [
                     {expand: true, flatten: true, src: 'tmp/holoplay/HoloPlay/holoplay.js', dest: 'lkg-viewer/js/deps/holoplay/'}
-                ]
-            },
-            gamepad: {
-                files: [
-                    {expand: true, flatten: true, src: 'tmp/GamePad/GamePad.js-add-holoplay/lib/*', dest: 'lkg-viewer/js/deps/GamePad/'}
                 ]
             }
         },
@@ -32,6 +24,22 @@ module.exports = function(grunt) {
                         js: {
                             'three/build/**/*.js': 'three/',
                             'three/examples/js/loaders/GLTFLoader.js': 'three/loaders/'
+                        }
+                    }
+                },
+                pkg: 'package.json',
+                dest: {
+                    js: 'lkg-viewer/js/deps/'
+                }
+            },
+            "holoplay-gamepad": {
+                options: {
+                    minified: false,
+                    unminified: false,
+                    include: {
+                        js: {
+                            'holoplay-gamepad/lib/**/*.js': 'holoplay-gamepad/',
+                            'uupaa.gamepad.js/lib/**/*.js': 'uupaa.gamepad.js/'
                         }
                     }
                 },
