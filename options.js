53756460f947fb79df46315cfddf2f29948398c8
Brian R. Jackson <brian@jaxzin.com>
2025-06-19 21:40:05 -0400

    fix: update viewer when clearing cached photos

--- options.js
+++ options.js
@@ -5,7 +5,7 @@ clearPhotos.onclick = function(element) {
             console.log("Clear photos database");
             chrome.tabs.query({title: 'Looking Glass Viewer for Facebook 3D Photos'}, function(tabs) {
                 var windows = chrome.extension.getViews({tabId: tabs[0].id});
-                windows[0].updatePhotos([]);
+                windows[0].viewer.updatePhotos([]);
             });
         }
     );
