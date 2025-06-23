52c418e63596c49fffdab328b79e4e2372c6557b
Brian R. Jackson <brian@jaxzin.com>
2025-06-23 16:45:04 -0400

    fix integration test and reporter path

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  reporters: ['default', 'jest-junit'],
+  reporters: ['default', '<rootDir>/node_modules/jest-junit/index.js'],
   moduleNameMapper: {
     '^./deps/three/examples/jsm/loaders/GLTFLoader.js$': '<rootDir>/tests/__mocks__/GLTFLoader.js',
     '^./deps/holoplay/holoplay.module.js$': '<rootDir>/tests/__mocks__/holoplay.module.js'
--- test/integration.test.js
+++ test/integration.test.js
@@ -1,12 +1,15 @@
 import puppeteer from 'puppeteer';
+import { jest } from '@jest/globals';
+
+jest.setTimeout(20000);
 
 let browser;
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox', '--allow-file-access-from-files', '--disable-dev-shm-usage'],
     executablePath: process.env.PUPPETEER_EXEC_PATH,
-    headless: false,
+    headless: true,
   });
 });
 
@@ -19,7 +22,7 @@ afterAll(async () => {
 test('viewer initializes', async () => {
   const page = await browser.newPage();
   const url = 'file://' + process.cwd() + '/lkg-viewer/index.html';
-  await page.goto(url);
+  await page.goto(url, { waitUntil: 'load' });
   await page.waitForSelector('canvas');
   const viewerExists = await page.evaluate(() => typeof window.viewer !== 'undefined');
   expect(viewerExists).toBe(true);
