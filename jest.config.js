73ed2d07a317a5c48c70bc67b7d38ba7e07ccd9b
Brian R. Jackson <brian@jaxzin.com>
2025-06-23 21:05:48 -0400

    Run integration tests via jest CLI

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 export default {
   testEnvironment: 'node',
   transform: {},
-  reporters: ['default', 'jest-junit'],
   moduleNameMapper: {
     '^./deps/three/examples/jsm/loaders/GLTFLoader.js$': '<rootDir>/tests/__mocks__/GLTFLoader.js',
     '^./deps/holoplay/holoplay.module.js$': '<rootDir>/tests/__mocks__/holoplay.module.js'
